555f54ea1be278d434464f5f67e45b9f6d8982de
burakcolak00 <burakcolak000@gmail.com>
2025-06-30 15:52:54 -0500

    final commit

--- src/test/java/com/csc/TestCheeseAnalyzer.java
+++ src/test/java/com/csc/TestCheeseAnalyzer.java
@@ -1,5 +1,35 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class TestCheeseAnalyzer {
-  // You can put test code here
+  @Test
+  public void testCountByMilkTreatmentType() {
+    CheeseList list = new CheeseList();
+    list.addCheese(new Cheese("1", "Cow", "low fat", "Pasteurized", 0, 40.0));
+    list.addCheese(new Cheese("2", "Goat", "low fat", "Raw Milk", 1, 42.0));
+    list.addCheese(new Cheese("3", "Cow", "low fat", "Pasteurized", 1, 45.0));
+    assertEquals(2, list.countByMilkTreatmentType("Pasteurized"));
+    assertEquals(1, list.countByMilkTreatmentType("Raw Milk"));
+  }
+
+  @Test
+  public void testCountOrganicHighMoisture() {
+    CheeseList list = new CheeseList();
+    list.addCheese(new Cheese("1", "Cow", "low fat", "Pasteurized", 1, 42.0));
+    list.addCheese(new Cheese("2", "Goat", "low fat", "Raw Milk", 1, 39.0));
+    list.addCheese(new Cheese("3", "Cow", "low fat", "Pasteurized", 0, 50.0));
+    assertEquals(1, list.countOrganicHighMoisture());
+  }
+
+  @Test
+  public void testMostCommonMilkType() {
+    CheeseList list = new CheeseList();
+    list.addCheese(new Cheese("1", "Cow", "low fat", "Pasteurized", 0, 40.0));
+    list.addCheese(new Cheese("2", "Goat", "low fat", "Raw Milk", 1, 42.0));
+    list.addCheese(new Cheese("3", "Cow", "low fat", "Pasteurized", 1, 45.0));
+    assertEquals("Cow", list.mostCommonMilkType());
+  }
 }
